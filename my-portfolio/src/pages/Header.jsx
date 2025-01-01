6efe31708e7a6e119a5c19ca942f5ff495344fe4
aeMyst <panpetertran123@gmail.com>
2024-12-31 23:39:20 -0800

    added socials

--- my-portfolio/src/pages/Header.jsx
+++ my-portfolio/src/pages/Header.jsx
@@ -74,7 +74,7 @@ const Header = () => {
             className="label cursor-pointer"
             onClick={() => handleNavigation('history')}
           >
-            Professional History
+            Experience
           </label>
         </div>
       </div>
